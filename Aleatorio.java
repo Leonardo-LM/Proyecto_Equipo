e644de141607ae0fef6bcdd84208655bfbd5a5f9
Diego Axel Contreras Guerrero <diegoaxelcontreras13@gmail.com>
2024-09-07 14:09:27 -0600

    Archivo Aleatorio Modificado

--- Aleatorio.java
+++ Aleatorio.java
@@ -11,3 +11,33 @@ public class Aleatorio {
         int residuo, division;
 
         System.out.println("Valor generado: " + random);
+
+        for (int i = arreglo.length - 1; i >= 0 && random != 0; i--) {
+
+            if (random % 2 == 0) {
+
+                arreglo[i] = 0;
+
+            } else {
+
+                arreglo[i] = 1;
+
+            }
+
+            random /= 2;
+
+        }//Fin del metodo for
+
+        if (random == 0) {
+
+            System.out.println("En el arreglo quedaron los valores: " + Arrays.toString(arreglo));
+
+        } else {
+
+            System.out.println("Celdas del arreglo se agotaron");
+
+        }
+
+    }//Fin del método main
+
+}
